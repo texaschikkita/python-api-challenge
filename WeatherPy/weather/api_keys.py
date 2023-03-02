f285753e60d6262fc667707e96e9ad02e9099b49
Jessica McPhaul <120339830+texaschikkita@users.noreply.github.com>
2023-03-02 16:51:11 -0600

    Add files via upload

---
+++ WeatherPy/weather/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "1f90ef8c31e3f91b7c6c53b27d133410"
+
+# Geoapify API Key
+geoapify_key = "0269ec4589a94fb6a24d939ecbc8ce29"
+geoapifykey2 = geoapify_key = "964bfa976dc34a8b85f14dcf50b3108e"
+
+# Gmaps API Key
+gmaps_key = "AIzaSyBD7lYj55wuuw7LeC2Lz-uX8hMxZJFAjKI"
